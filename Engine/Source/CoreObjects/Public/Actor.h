806c75257414286db9d96f3edf25d21c3df91a32
조용현 <quietstring@overdare.com>
2025-09-10 19:25:08 +0900

    WIP meta data inheritance not working

---
+++ Engine/Source/CoreObjects/Public/Actor.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Object.h"
+#include "qmeta_macros.h"
+
+class QActor : public QObject
+{
+public:
+    QPROPERTY()
+    std::string Temp1;
+
+    QPROPERTY()
+    std::string Temp2;
+};
